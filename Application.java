b6011431080203804b917efe1da492eda312e3aa
MiguelCTercero <Miguel.C.Tercero@Gmail.com>
2022-12-23 06:43:27 -0500

    Added Application to call the Calculator

---
+++ Application.java
@@ -0,0 +1,7 @@
+public class Application{
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.addition(1, 2));
+        System.out.println(calc.addition(1.5,3.2));
+    }
+}
